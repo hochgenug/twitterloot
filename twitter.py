a440de85beed3f53964ca37d13827bab8a044766
Bruno Faugeroux <clowrid@gmail.com>
2022-02-09 19:29:48 +0100

    Clean code

--- twitter.py
+++ twitter.py
@@ -24,97 +24,73 @@ f = open(filepath, "a+")
 regex = r"(^|[^@\w])@(\w{1,30})"
 newtweetcounter, oldtweetcounter, ignorecounter = 0, 0, 0
 
-for tweet in tweepy.Cursor(api.search, q="concours RT",
-                           lang="fr", result_type='popular', count=int(os.getenv('SEARCH_ITEM_NUMBER')),
-                           tweet_mode='extended').items(int(os.getenv('SEARCH_ITEM_NUMBER'))):
+searchwordlist =  os.getenv('SEARCH_WORDS').split(",")
+for searchword in searchwordlist:
+    for tweet in tweepy.Cursor(api.search, q=searchword,
+                            lang="fr", result_type='popular', count=int(os.getenv('SEARCH_ITEM_NUMBER')),
+                            tweet_mode='extended').items(int(os.getenv('SEARCH_ITEM_NUMBER'))):
 
-    if (tweet.user.screen_name == '@topito_com' or tweet.user.screen_name == 'topito_com'):
-        continue
+        if (tweet.user.screen_name == '@topito_com' or tweet.user.screen_name == 'topito_com'):
+            continue
 
-    if int(tweet.id_str) in listTweetID:
-        if int(os.getenv('DEBUG')) == 1:
-            print("tweet already processed : ",int(tweet.id_str))
-        oldtweetcounter += 1
-        continue
-    elif (tweet.in_reply_to_status_id is not None) or hasattr(tweet, 'retweeted_status'):
-        if int(os.getenv('DEBUG')) == 1:
-            print("tweet is a reply/RT : ",int(tweet.id_str))
-        ignorecounter += 1
-        continue
+        if int(tweet.id_str) in listTweetID:
+            if int(os.getenv('DEBUG')) == 1:
+                print("tweet already processed : ",int(tweet.id_str))
+            oldtweetcounter += 1
+            continue
+        elif (tweet.in_reply_to_status_id is not None) or hasattr(tweet, 'retweeted_status'):
+            if int(os.getenv('DEBUG')) == 1:
+                print("tweet is a reply/RT : ",int(tweet.id_str))
+            ignorecounter += 1
+            continue
 
-    else:
-        f.write(str(int(tweet.id_str)) + "\n")
-        matches = re.findall(regex, tweet.full_text)
-        newtweetcounter += 1
-        for match in matches:
-            print("Follow @%s" % (match[1]))
+        else:
+            f.write(str(int(tweet.id_str)) + "\n")
+            matches = re.findall(regex, tweet.full_text)
+            newtweetcounter += 1
+            for match in matches:
+                print("Follow @%s" % (match[1]))
+                try:
+                    api.create_friendship(match[1])
+                except Exception as e:
+                    print(e)
+                    pass
+
             try:
-                api.create_friendship(match[1])
+                api.create_favorite(tweet.id)
+                if (tweet.full_text.lower().find('tag') != -1 or tweet.full_text.lower().find('identifie') != -1  or tweet.full_text.lower().find('mentionne') != -1 or tweet.full_text.lower().find('ami') != -1 or tweet.full_text.lower().find('comment') != -1):
+                    api.update_status(status = '@ramasmaf ça peut t interesser', in_reply_to_status_id = tweet.id , auto_populate_reply_metadata=True)
             except Exception as e:
                 print(e)
                 pass
 
-        try:
-            api.create_favorite(tweet.id)
-            if (tweet.full_text.lower().find('tag') != -1 or tweet.full_text.lower().find('identifie') != -1  or tweet.full_text.lower().find('mentionne') != -1 or tweet.full_text.lower().find('ami') != -1 or tweet.full_text.lower().find('comment') != -1):
-                api.update_status(status = '@ramasmaf ça peut t interesser', in_reply_to_status_id = tweet.id , auto_populate_reply_metadata=True)
-                print("Maf was taggued : ",int(tweet.id_str))
-        except Exception as e:
-            print(e)
-            pass
+            try:
+                api.create_friendship(tweet.user.screen_name)
+            except Exception as e:
+                print(e)
+                pass
 
-        try:
-            api.create_friendship(tweet.user.screen_name)
-        except Exception as e:
-            print(e)
-            pass
+            try:
+                tweet.retweet()
+            except Exception as e:
+                print(e)
+                pass
 
+print('Subscribed to',newtweetcounter,'new contests.',oldtweetcounter,'were ignored (already subscribed)',ignorecounter,'were ignored (reply or RT)')
+
+################# RT alternatives topics to hide competition RT #################
+itemlist =  os.getenv('ALTERNATIVE_TOPICS').split(",")
+#print(itemlist)
+for item in itemlist:
+    for tweet in tweepy.Cursor(api.search, q=item,
+                           lang="fr", result_type='popular', count=int(os.getenv('ALTERNATIVE_ITEM_NUMBER')),
+                           tweet_mode='extended').items(int(os.getenv('ALTERNATIVE_ITEM_NUMBER'))):
+        f.write(str(int(tweet.id_str)) + "\n")
         try:
             tweet.retweet()
         except Exception as e:
             print(e)
             pass
 
-print('Subscribed to',newtweetcounter,'new contests.',oldtweetcounter,'were ignored (already subscribed)',ignorecounter,'were ignored (reply or RT)')
-
-for tweet in tweepy.Cursor(api.search, q="Demon slayer",
-                           lang="fr", result_type='popular', count=5,
-                           tweet_mode='extended').items(5):
-    f.write(str(int(tweet.id_str)) + "\n")
-    try:
-        tweet.retweet()
-    except Exception as e:
-        print(e)
-        pass
-
-for tweet in tweepy.Cursor(api.search, q="Fate Stay Night",
-                           lang="fr", result_type='popular', count=5,
-                           tweet_mode='extended').items(5):
-    f.write(str(int(tweet.id_str)) + "\n")
-    try:
-        tweet.retweet()
-    except Exception as e:
-        print(e)
-        pass
-
-for tweet in tweepy.Cursor(api.search, q="Symfony php",
-                           lang="fr", result_type='popular', count=5,
-                           tweet_mode='extended').items(5):
-    f.write(str(int(tweet.id_str)) + "\n")
-    try:
-        tweet.retweet()
-    except Exception as e:
-        print(e)
-        pass
-
-for tweet in tweepy.Cursor(api.search, q="attaque des titans",
-                           lang="fr", result_type='popular', count=5,
-                           tweet_mode='extended').items(5):
-    f.write(str(int(tweet.id_str)) + "\n")
-    try:
-        tweet.retweet()
-    except Exception as e:
-        print(e)
-        pass
 
 f.close()
